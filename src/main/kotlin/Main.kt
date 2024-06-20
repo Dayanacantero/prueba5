4437fc4da433fd77d0bee5dfeda33f0b6be78a8e
Dayanacantero <astridcantero2005@gmail.com>
2024-06-20 09:29:46 -0500

    subir

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,186 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    // fun main() {
+    //   val trickFunction = trick
+    // }
+
+    //fun trick() {
+    //   println("No treats!")
+    // }
+
+    //  val trickFunction = ::trick
+//}
+
+//fun trick() {
+    //  println("No treats!")
+
+    //  val trickFunction = ::trick
+//}
+
+//val trick = {
+    //  println("No treats!")
+
+    //  val trickFunction = trick
+//}
+
+//val trick = {
+    // println("No treats!")
+
+    // val trickFunction = trick
+    // trick()
+//}
+
+//val trick = {
+    // println("No treats!")
+
+    // val trickFunction = trick
+    // trick()
+    //  trickFunction()
+//}
+
+//    val trickFunction = trick
+    //  trick()
+    //  trickFunction()
+    // treat()
+//}
+//val trick = {
+    // println("No treats!")
+//}
+
+    //val treat = {
+//    println("Have a treat!")
+
+
+    //val treat: () -> Unit = {
+    // println("Have a treat!")
+    //  }
+
+    //val treatFunction = trickOrTreat(false)
+    //val trickFunction = trickOrTreat(true)
+    //treatFunction()
+    //  trickFunction()
+    //}
+
+    //fun trickOrTreat(isTrick: Boolean): () -> Unit {
+    //if (isTrick) {
+    //   return trick
+    // } else {
+    //       return treat
+    //     }
+    //}
+
+
+    // val trick = {
+    //   println("No treats!")
+    // }
+
+    // val treat = {
+    //      println("Have a treat!")
+//    }
+
+    // val coins: (Int) -> String = { quantity ->
+    //  "$quantity quarters"
+    //}
+
+    //  val treatFunction = trickOrTreat(false)
+    //  val trickFunction = trickOrTreat(true)
+    //  treatFunction()
+    //  trickFunction()
+//}
+    //val coins: (Int) -> String = { quantity ->
+    //  "$quantity quarters"
+    //}
+
+    // val cupcake: (Int) -> String = { quantity ->
+    //    "Have a cupcake!"
+    // }
+    // val cupcake: (Int) -> String = {
+    //   "Have a cupcake!"
+    //   }
+
+//    val coins: (Int) -> String = { quantity ->
+//        "$quantity quarters"
+//    }
+
+//    val coins: (Int) -> String = {
+//        "$quantity quarters"
+//    }
+
+//    val coins: (Int) -> String = {
+//        "$quantity quarters"
+//    }
+
+
+//
+//    val cupcake: (Int) -> String = {
+//        "Have a cupcake!"
+//    }
+//
+//    val treatFunction = trickOrTreat(false, coins)
+//    val trickFunction = trickOrTreat(true, cupcake)
+    //treatFunction()
+    //  trickFunction()
+//}
+
+//    val treatFunction = trickOrTreat(false, { "$it quarters" })
+//    val trickFunction = trickOrTreat(true, null)
+//    treatFunction()
+//    trickFunction()
+//}
+//fun trickOrTreat(isTrick: Boolean, extraTreat: (Int) -> String): () -> Unit {
+    //  if (isTrick) {
+    //   return trick
+    // } else {
+    //  println(extraTreat(5))
+    //    return treat
+
+    //val coins: (Int) -> String = { quantity ->
+    //   "$quantity quarters"
+
+// }
+// fun main() {
+    // }
+    // val coins: (Int) -> String = {
+    //   "$it quarters"
+    // }
+
+    // val treatFunction = trickOrTreat(false, coins)
+    //val trickFunction = trickOrTreat(true, null)
+    //treatFunction()
+    // trickFunction()
+    //}
+
+//    val treatFunction = trickOrTreat(false) { "$it quarters" }
+//    val trickFunction = trickOrTreat(true, null)
+//    treatFunction()
+//    trickFunction()
+//    repeat(4) {
+//
+//    }
+//    trickFunction()
+//}
+
+    // fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    // if (isTrick) {
+    // return trick
+    //   } else {
+    //   if (extraTreat != null) {
+    //      println(extraTreat(5))
+    //      }
+    //   return treat
+    //   }
+}
+
+
+
+
+
+
+
+
+
+
+
